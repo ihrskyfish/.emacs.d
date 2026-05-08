eee04129c4b810f9d9c2fa9d497b8e9145b7d078
ihrskyfish <ihr17867950748@126.com>
2026-05-08 10:47:42 +0800

    pass ,but problem

--- init.el
+++ init.el
@@ -138,11 +138,15 @@
 ;; 
 (use-package rime
     :ensure t
-  :custom			      
+    :custom
+    
     ;; (default-input-method "rime")
       (rime-user-data-dir "~/.config/ibus/rime")  ;; 共享系统 Rime 配置
   ;; (rime-librime-root "~/.emacs.d/librime/dist")  ;; 编译好的 librime 路径
-  (rime-show-candidate 'popup)
+      (rime-show-candidate 'popup)
+  :bind
+  (:map rime-mode-map
+        ("C-`" . rime-send-keybinding))  ;; 发送 Rime 快捷键（如 ` 切换方案）      
   )  ;; TUI 下用 minibuffer 显示候选
 
 
