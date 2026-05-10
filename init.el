30e738932ae6b1839636a4bb71ebd05a90d10d50
ihrskyfish <ihr17867950748@126.com>
2026-05-10 13:49:36 +0800

    package rime

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(manoj-dark))
- '(package-selected-packages '(chinese-number chinese-wbim pyim-wbdict rime vterm)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -152,10 +152,9 @@
   ("C-`" . rime-send-keybinding)
   )  ;; TUI 下用 minibuffer 显示候选
 
-;; (use-package librime
-;;     :ensure t
-   
-;;   )
+(use-package liberime
+    :ensure t
+      )
 
 
 (use-package pyim
