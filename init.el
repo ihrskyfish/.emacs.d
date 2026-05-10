ffb4c841cc718e60d66bdadc969bd5e63a1c959f
ihrskyfish <ihr17867950748@126.com>
2026-05-11 07:24:41 +0800

    add key

--- init.el
+++ init.el
@@ -19,8 +19,14 @@
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 
+
+(defun open-jjjj_yn()
+  (interactive)
+  (find-file "~/mycode/jjjj_yn/jjjj_yn.org"))
+
 ;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
 (global-set-key (kbd "<f2>") 'open-init-file)
+(global-set-key (kbd "C-<f2>") 'open-jjjj_yn)
 
 
 ;; 确保 use-package 可用
